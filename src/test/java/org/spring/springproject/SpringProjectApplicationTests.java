1bf6aa647cd6740b26d742579ddc8b04d21d8d1b
jwalasik3 <walasikjakub@gmail.com>
2024-05-01 12:44:54 +0200

    First build/run of Spring application, smooth af

---
+++ src/test/java/org/spring/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
